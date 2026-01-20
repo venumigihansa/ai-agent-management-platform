wso2/agent-manager

Improve terminal color scheme for maximum compatibility
Updated color scheme in install.sh and uninstall.sh to use 8-bit color
mode for maximum compatibility across all terminal types.

Changes:
- Replaced 256-color mode with basic 8-bit ANSI colors
- Updated color codes for universal compatibility:
  - RED: \033[1;31m (bold red)
  - GREEN: \033[1;32m (bold green)
  - YELLOW: \033[1;33m (bold yellow)
  - ORANGE: \033[1;33m (bold yellow - same as warning)
- Ensures consistent color scheme across both scripts

The 8-bit color mode works on ALL terminals including basic 8-color
terminals, providing 100% portability across Unix/Linux/Mac systems
without requiring special terminal capabilities. Bold yellow is used
for both info and warning messages (distinguished by icons ℹ vs ⚠).

deployments/quick-start/install.sh
@@ -32,16 +32,16 @@ TIMEOUT_DATA_PLANE=600
 TIMEOUT_BUILD_PLANE=600
 TIMEOUT_OBSERVABILITY_PLANE=900
 
-# Colors for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
+# Colors for output (8-bit mode for maximum compatibility)
+RED='\033[1;31m'
+GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+ORANGE='\033[1;33m'
+NC='\033[0m'
 
 # Helper functions
 log_info() {
-    echo -e "${BLUE}ℹ${NC} $1"
+    echo -e "${ORANGE}ℹ${NC} $1"
 }
 
 log_success() {
@@ -58,9 +58,9 @@ log_error() {
 
 log_step() {
     echo ""
-    echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-    echo -e "${BLUE}$1${NC}"
-    echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+    echo -e "${ORANGE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+    echo -e "${ORANGE}$1${NC}"
+    echo -e "${ORANGE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
     echo ""
 }
 

deployments/quick-start/uninstall.sh
@@ -28,16 +28,16 @@ BUILD_CI_NS="${BUILD_CI_NS:-openchoreo-build-plane}"
 OBSERVABILITY_NS="${OBSERVABILITY_NS:-openchoreo-observability-plane}"
 DEFAULT_NS="${DEFAULT_NS:-default}"
 
-# Colors for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
+# Colors for output (8-bit mode for maximum compatibility)
+RED='\033[1;31m'
+GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+ORANGE='\033[1;33m'
+NC='\033[0m'
 
 # Helper functions
 log_info() {
-    echo -e "${BLUE}ℹ${NC} $1"
+    echo -e "${ORANGE}ℹ${NC} $1"
 }
 
 log_success() {
@@ -54,9 +54,9 @@ log_error() {
 
 log_step() {
     echo ""
-    echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-    echo -e "${BLUE}$1${NC}"
-    echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+    echo -e "${ORANGE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+    echo -e "${ORANGE}$1${NC}"
+    echo -e "${ORANGE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
     echo ""
 }
 

venumigihansa/ai-agent-management-platform

deployments/quick-start/install.sh
@@ -32,16 +32,15 @@ TIMEOUT_DATA_PLANE=600
 TIMEOUT_BUILD_PLANE=600
 TIMEOUT_OBSERVABILITY_PLANE=900
 
-# Colors for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
+# Colors for output (8-bit mode for maximum compatibility)
+RED='\033[1;31m'
+GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 # Helper functions
 log_info() {
-    echo -e "${BLUE}ℹ${NC} $1"
+    echo -e "${NC}ℹ${NC} $1"
 }
 
 log_success() {
@@ -58,9 +57,9 @@ log_error() {
 
 log_step() {
     echo ""
-    echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-    echo -e "${BLUE}$1${NC}"
-    echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+    echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+    echo -e "${NC}$1${NC}"
+    echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
     echo ""
 }
 

deployments/quick-start/uninstall.sh
@@ -28,16 +28,15 @@ BUILD_CI_NS="${BUILD_CI_NS:-openchoreo-build-plane}"
 OBSERVABILITY_NS="${OBSERVABILITY_NS:-openchoreo-observability-plane}"
 DEFAULT_NS="${DEFAULT_NS:-default}"
 
-# Colors for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
+# Colors for output (8-bit mode for maximum compatibility)
+RED='\033[1;31m'
+GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 # Helper functions
 log_info() {
-    echo -e "${BLUE}ℹ${NC} $1"
+    echo -e "${NC}ℹ${NC} $1"
 }
 
 log_success() {
@@ -54,9 +53,9 @@ log_error() {
 
 log_step() {
     echo ""
-    echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-    echo -e "${BLUE}$1${NC}"
-    echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+    echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+    echo -e "${NC}$1${NC}"
+    echo -e "${NC}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
     echo ""
 }
 